6bd593d162a40f8cb24192721b279bf8af85d9de
eugeneproverdin <eugeneverdinpro@gmail.com>
2018-03-26 09:20:03 +0200

    c'estbon

--- .build/mzero_pro_bl_dbg/sketch/main.ino.cpp
+++ .build/mzero_pro_bl_dbg/sketch/main.ino.cpp
@@ -1,9 +1,16 @@
+#line 1 "c:\\Users\\eleve\\Desktop\\ganeugene\\arduino\\main.ino"
+#line 1 "c:\\Users\\eleve\\Desktop\\ganeugene\\arduino\\main.ino"
                                                                                                                                                                                                                                                                                     //  Copyright (c) 2018 Antoine Tran Tan
 //
 
 #include "Arduino.h"
 
 // the setup function runs once when you press reset or power the board
+#line 7 "c:\\Users\\eleve\\Desktop\\ganeugene\\arduino\\main.ino"
+void setup();
+#line 12 "c:\\Users\\eleve\\Desktop\\ganeugene\\arduino\\main.ino"
+void loop();
+#line 7 "c:\\Users\\eleve\\Desktop\\ganeugene\\arduino\\main.ino"
 void setup()
 {
 }
@@ -14,3 +21,4 @@ void loop()
     static int potPosition;
     potPosition = analogRead(A1);
 }
+
